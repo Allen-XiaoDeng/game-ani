8a000be246db57122d7fe5aae012ce6542295093
Allen <1942813644@qq.com>
2022-04-05 12:04:54 +0800

    完成游戏角色滚动绑定动画

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger); // register the plugin
 
 gsap.to("#logo", {
     y: 60,
@@ -18,5 +19,17 @@ gsap.from(".release", {
     x: 120,
     opacity: 0,
     duration: 2,
-
+})
+gsap.to(".first", {
+    rotateX: "45deg",
+    skewX: '-25deg',
+    TransformOrigin: "bottom",
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".first",
+        start: "top 25%",
+        end: "top top",
+        markers: true,
+        scrub: true,
+    }
 })
